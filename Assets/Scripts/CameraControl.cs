cb047d391eef8e190f55f35696df048c2a8e9d28
Chengming Lu <cluster626@gmail.com>
2016-09-28 22:25:37 -0400

    Create basic game objects / Optimized CameraControl
    Added core game units, added sprites, box colliders and empty scripts.
    Prefabbed

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -3,9 +3,8 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour {
     float CameraThreshold = 2f;
+    float CameraBoundary = 5f;
     Camera cam;
-    int Horizontal = 1;
-    int Vertical = 1;
     float speed = 2f;
 	// Use this for initialization
 	void Start () {
@@ -16,22 +15,24 @@ public class CameraControl : MonoBehaviour {
         Debug.Log("Mouse:" +Input.mousePosition+ ";pos:"+transform.position+";Cam:" +
             cam.orthographicSize + ";screen"+Screen.width + " " + Screen.height);
 
-        if (transform.position.x > 0 && transform.position.x < Screen.width
-            && transform.position.y > 0 && transform.position.y < Screen.height) {
-
+        if (Mathf.Abs(Input.mousePosition.x - Screen.width) <= CameraThreshold
+            && transform.position.x <= CameraBoundary) {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);  
         }
-                    if (Mathf.Abs(Input.mousePosition.x - Screen.width) <= CameraThreshold) {
-                transform.Translate(Vector3.right * speed * Time.deltaTime);  
-            }
-            if (Input.mousePosition.x <= CameraThreshold) {
-                transform.Translate(Vector3.left * speed * Time.deltaTime);  
-            }
-            if (Mathf.Abs(Input.mousePosition.y - Screen.height) <= CameraThreshold) {
-                transform.Translate(Vector3.up * speed * Time.deltaTime);  
-            }
-            if (Input.mousePosition.y <= CameraThreshold) {
-                transform.Translate(Vector3.down * speed * Time.deltaTime);  
-            }
+        if (Input.mousePosition.x <= CameraThreshold
+            && transform.position.x >= -CameraBoundary) {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);  
+        }
+        if (Mathf.Abs(Input.mousePosition.y - Screen.height) <= CameraThreshold
+            && transform.position.y <= CameraBoundary) {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);  
+        }
+        if (Input.mousePosition.y <= CameraThreshold
+            && transform.position.y >= -CameraBoundary) {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);  
+        }
+        
+                    
 
         //float y = Vertical * Input.GetAxis("Mouse Y");
         
