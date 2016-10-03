7154943ae9705c26d2bda4d2b50e48ad38594ebf
Chengming Lu <cluster626@gmail.com>
2016-10-02 21:03:49 -0400

    Able to move units
    Majority of previous issues fixed

--- Assets/Scripts/Movable.cs
+++ Assets/Scripts/Movable.cs
@@ -4,13 +4,20 @@ using System.Collections;
 public class Movable : MonoBehaviour {
     public float speed;
     public Vector3 destination;
+    private Vector3 moveThreshold = new Vector3(0.1f, 0.1f, 0);
 	// Use this for initialization
 	void Start () {
-        destination = transform.position;
+        destination = gameObject.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if (Mathf.Abs(destination.x - gameObject.transform.position.x) >= moveThreshold.x || 
+            Mathf.Abs(destination.y - gameObject.transform.position.y) >= moveThreshold.y) {
+            //Debug.Log(mousPos + " is mouse position:moving");
+            Debug.Log("in movable: " + destination + " is object position:moving");
+            gameObject.transform.Translate(((destination - 
+                gameObject.transform.position) * speed * Time.deltaTime));
+        }
 	}
 }
--- Assets/Scripts/Orderable.cs
+++ Assets/Scripts/Orderable.cs
@@ -7,50 +7,33 @@ using System.Collections;
 public class Orderable : MonoBehaviour {
     public GameObject basePrefab;
     public static GameObject selectedUnit;
-    public bool selected = false;
+    //private bool selected;
     private Vector3 mousPos, objPos2D;
-    private GameObject tempObject;
+
+    //private GameObject tempObject;
 	// Use this for initialization
 	void Start () {
-        tempObject = gameObject;
+        //selected = false;
+        //tempObject = gameObject;
 	}
-	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetMouseButtonDown(1)) {
+            Debug.Log("Right click detected, selected unit is " + selectedUnit);
             if (selectedUnit != null) {
                 mousPos = Camera.main.ScreenToWorldPoint(
                     new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
                 mousPos.z = 0;
-                selectedUnit.GetComponent<Movable>().destination = 
-                    new Vector3(selectedUnit.transform.position.x,
-                    selectedUnit.transform.position.y, 0);
-                Debug.Log(mousPos + " is mouse position");
-                Debug.Log("Moving object " + selectedUnit);           
+                Debug.Log("Mouse position is " + mousPos);
+                selectedUnit.GetComponent<Movable>().destination = mousPos;
+                Debug.Log("Position for " + gameObject + " is set to " + 
+                    gameObject.GetComponent<Movable>().destination);         
             }
         }
-        Vector3 moveThreshold = new Vector3(0.1f, 0.1f, 0);
-        if (Mathf.Abs(mousPos.x - objPos2D.x) >= moveThreshold.x || 
-            Mathf.Abs(mousPos.y - objPos2D.y) >= moveThreshold.y) {
-            Debug.Log(mousPos + " is mouse position:moving");
-            Debug.Log(objPos2D + " is object position:moving");
-            selectedUnit.transform.Translate(((mousPos - objPos2D) * 
-            selectedUnit.GetComponent<Movable>().speed * Time.deltaTime));
-            objPos2D = new Vector3(selectedUnit.transform.position.x,
-                selectedUnit.transform.position.y, 0);
-            Debug.Log("position x is: " + selectedUnit.transform.position.x + "position z is "
-                + selectedUnit.transform.position.z);
-           }
 	}
-
-    void Move() {
-
-    }
     void OnMouseDown() {
         selectedUnit = gameObject;
-        tempObject = gameObject;
-        gameObject.GetComponent<Orderable>().selected = true;
+        //tempObject = gameObject;
         Debug.Log("Selected unit assigned to " + selectedUnit);
     }
 }
-
